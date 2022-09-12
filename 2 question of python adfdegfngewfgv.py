7314694c5606d087f5012953b4324d69a51995e4
chandankumar0160 <112537104+chandankumar0160@users.noreply.github.com>
2022-09-12 10:53:16 +0530

    Add files via upload

---
+++ 2 question of python adfdegfngewfgv.py
@@ -0,0 +1,12 @@
+num=int(input("enter a number:"))
+x=num
+rev=0
+while num>0:
+    r=num&10
+    rev=(rev*10)+r
+    num=num//10
+print("reverse given number is ",rev)
+if x==rev:
+    print("the num is a palindrome")
+else:
+    print("the num is not a palindrome")
